9b1e9404539870ce5db8b8c48e98ad2e55ddb798
hagi torres <hagiraitorresmacedo@gmail.com>
2022-04-22 03:38:58 -0500

    deploy 2.8

--- Front-end/src/services/post/login.js
+++ Front-end/src/services/post/login.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const login = async (customer) => {
     console.log( {customer});
     const {email,password} = customer;
-    let url = `http://localhost:3001/customer/login`;
+    let url = `http://15.228.78.162:3001/customer/login`;
     return await axios.post(url, {email,password})
         
 }
--- Front-end/src/services/post/registry.js
+++ Front-end/src/services/post/registry.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const createCustomer = async (customer) => {
     const {email,password} = customer;
     let data2;
-    let url = "http://localhost:3001/customer/create";
+    let url = "http://15.228.78.162:3001/customer/create";
     return await axios.post(url, {email,password})
         
 }
